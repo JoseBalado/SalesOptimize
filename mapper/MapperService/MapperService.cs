8d29a24685aa2ef95a1d566d33c3f73afcb5ff54
Jose Balado <JoseBalado@users.noreply.github.com>
2021-01-06 13:57:26 +0100

    Passing some basic tests

--- mapper/MapperService/MapperService.cs
+++ mapper/MapperService/MapperService.cs
@@ -2,7 +2,16 @@
 
 namespace MapperService
 {
-    public class Class1
+    public class PrimeService
     {
+        public bool IsPrime(int candidate)
+        {
+            if (candidate < 2)
+            {
+                return false;
+            }
+            throw new NotImplementedException("Not fully implemented.");
+        }
     }
+
 }
