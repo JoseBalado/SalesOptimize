deb01b69a82594cb9a8d7eba287ddb22278b2c71
Jose Balado <JoseBalado@users.noreply.github.com>
2021-01-08 00:39:47 +0100

    First commit for PaperRulez, reading a file and writing it to console.

---
+++ PaperRulez/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace PaperRulez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Open the text file using a stream reader.
+                using (var sr = new StreamReader("./client/1234_test.txt"))
+                {
+                    // Read the stream as a string, and write the string to the console.
+                    Console.WriteLine(sr.ReadToEnd());
+                }
+            }
+
+            catch (IOException e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
+
